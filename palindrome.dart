24bf157b8f5e4f31a166fd96347377f8a2617800
Marcela <marcelacrv@icloud.com>
2022-10-04 14:48:19 -0300

    Update palindrome.dart

--- palindrome.dart
+++ palindrome.dart
@@ -4,7 +4,6 @@ void main() {
   List<int> numbers = [10, 2, 0, 1, 2, 10];
   bool isPalindrome = false;
   
-  
   void verifyListNumbers() {
     for(int i = 0; i < numbers.length; i++) {
     if(numbers[i] == numbers[numbers.length - 1 - i]) {
